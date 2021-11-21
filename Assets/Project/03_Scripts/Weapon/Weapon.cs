e6a3b5e9a5b1a01ec2ab75ed206550b42b5f9adb
Ymizuta <suiden.53@gmail.com>
2021-11-21 18:40:38 +0900

    発射間隔を実装

--- Assets/Project/03_Scripts/Weapon/Weapon.cs
+++ Assets/Project/03_Scripts/Weapon/Weapon.cs
@@ -8,27 +8,67 @@ public class Weapon : MonoBehaviour
 	[SerializeField] private bool useMagazine = true;
 	[SerializeField] private int maxMagazineSize = 30;
 	[SerializeField] private bool autoReload = true;
-
 	public Character WeaponOwner { get; set; }
 	public WeaponAmmo WeaponAmmo { get; set; }
 	public int MaxMagazineSize => maxMagazineSize;
 	public bool UseMagazine => useMagazine;
 	public int CurrentAmmo { get; set; }
+	public bool CanShoot { get; set; }
+
+	private float nextShootTime;
 
 	private void Awake()
 	{
 		WeaponAmmo = this.gameObject.GetComponent<WeaponAmmo>();
 	}
 
-	public void TriggerShoot()
+	private void Update()
 	{
-		Shoot();
+		WeaponCanShoot();
 	}
 
-	private void Shoot()
+	public void TriggerShoot()
 	{
-		Debug.Log("Shooting");
+		StartShooting();
+	}
+
+	private void StartShooting()
+	{
+		if (useMagazine)
+		{
+			if (WeaponAmmo.CanUseWeapon())
+			{
+				RequestShoot();
+			}else if (autoReload)
+			{
+				Reload();
+			}
+		}
+		else
+		{
+			RequestShoot();
+		}
+	}
+
+	private void RequestShoot()
+	{
+		if (!CanShoot)
+		{
+			return;
+		}
+
+		Debug.Log($"Shooting Ammo : {CurrentAmmo}");
 		WeaponAmmo.ConsumeAmmo();
+		CanShoot = false;
+	}
+
+	private void WeaponCanShoot()
+	{
+		if (Time.time >= nextShootTime)
+		{
+			CanShoot = true;
+			this.nextShootTime = Time.time + timeBtwShoot;
+		}
 	}
 
 	public void Reload()
